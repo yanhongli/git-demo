5312c53a06e3221d416b76fcbaf54bfd16088d06
liyanhong01 <liyanhong01@zuoyebang.com>
2020-06-28 17:00:24 +0800

    npm

---
+++ .stylelintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    plugins: ['stylelint-order', 'stylelint-scss'],
+
+    extends: 'stylelint-config-standard',
+
+    rules: {
+
+        'at-rule-no-unknown': null,
+
+        'no-descending-specificity': null
+
+    }
+
+}
